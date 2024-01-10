f355f20ba5a1eaa6e037094c3ec1a47bdcb9cad2
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-01-10 10:51:53 -0300

    algunas modificaciones

---
+++ frontend/src/componentes/WhyUs/WhyUs.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Items from "../Items/Items";
+import exp from "../../multimedia/experiencia.png";
+import tech from "../../multimedia/mecanico.png";
+import atencion from "../../multimedia/equipo.png";
+import confianza from '../../multimedia/confianza.png';
+import planta from '../../multimedia/planta.png'
+const WhyUs = () => {
+  const datosComponenteB = [
+    {
+      img: exp,
+      text: "Contamos con una amplia experiencia en el ámbito geofísico y de ingeniería, lo que nos convierte en referentes en el sector.",
+      title: "Experiencia y trayectoria internacional",
+    },
+    {
+      img: tech,
+      text: "Mediante la integración de tecnología no intrusiva y sustentable, obtenemos una gran cantidad de datos que nos permite transformarla en soluciones prácticas para nuestros clientes.",
+      title: "Tecnología de Vanguardia",
+    },
+    {
+      img: atencion,
+      text: "Nuestro enfoque personalizado garantiza que recibas soluciones confiables y adaptadas a tus objetivos y requisitos.",
+      title: "Atención Personalizada",
+    },
+    {
+      img: planta,
+      text: "Nos esforzamos por ofrecer soluciones geofísicas que minimicen el impacto ambiental y promuevan la sustentabilidad y resiliencia en la industria de la construcción.",
+      title: "Enfoque Sustentable",
+    },
+    {
+      img: confianza,
+      text: "En FZ Obras, entendemos que la confianza y confidencialidad es esencial en cualquier relación comercial. Aquí están las razones para elegirnos como tu socio confiable:",
+      title: "Confianza y Credibilidad Garantizadas",
+    },
+  ];
+
+  return (
+    <div className="w-full flex flex-wrap justify-center items-center py-8">
+      <div className="w-full flex flex-wrap justify-center items-center py-6 mb-4">
+        <div className="w-full">
+          <h1 className="text-green-800 text-3xl font-barlow-400 tracking-widest">
+            F Z Obras
+          </h1>
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="w-2/5">
+            <hr className="my-2 border-[1.5px] border-green-500" />
+          </div>
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <h1 className="w-4/4 text-5xl font-barlow-400">
+            ¿Por qué Elegirnos?
+          </h1>
+        </div>
+      </div>
+      <div className="w-full py-12 flex justify-center items-center">
+        {/* <motion.h1
+        className="font-outfit-800 text-center md:text-left text-4xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl mb-0 md:mb-6 ml-0 md:ml-6"
+        style={{ color: "#001659" }}
+        variants={titleVariants}
+        initial="offscreen"
+        // animate="onscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        Nuestros Mercados
+      </motion.h1> */}
+        <div className="flex flex-wrap justify-center md:max-w-[1300px]">
+          {datosComponenteB.map((datos, index) => (
+            <div key={index} className="w-full md:w-1/3 p-4">
+              <Items img={datos.img} text={datos.text} title={datos.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyUs;
--- frontend/src/views/AboutUs/AboutUs.jsx
+++ frontend/src/views/AboutUs/AboutUs.jsx
@@ -2,53 +2,18 @@ import React from "react";
 import FrameContact from "../../componentes/Frame/FrameContact";
 import Header from "../../componentes/Header/Header";
 import Footer from "../../componentes/Footer/Footer";
-import img1 from "../../multimedia/slide1.jpg";
+import Impulse from "../../componentes/Impulse/Impulse";
+import ContactUs from "../../componentes/ContactUs/ContactUs";
+import WhyUs from "../../componentes/WhyUs/WhyUs";
 const AboutUs = () => {
   return (
     <div>
       <FrameContact />
       <Header />
-      <div className="w-full flex flex-wrap justify-center items-center py-8">
-        <div className="w-full flex flex-wrap justify-center items-center py-6 mb-4">
-          <div className="w-full">
-            <h1 className="w-full text-6xl font-barlow-500">F Z Obras</h1>
-          </div>
-          <div className="w-full flex justify-center">
-            <div className="w-2/5">
-              <hr className="my-2 border-[1.5px] border-green-500" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center">
-            <h1 className="w-2/3 text-3xl font-barlow-300">
-              Impulsando el Éxito de tu Proyecto con Soluciones Sustentables
-            </h1>
-          </div>
-        </div>
-        <div className="flex justify-center items-start px-14 space-x-4 my-10">
-          <div className="w-1/2 flex flex-wrap justify-center text-left text-xl font-barlow-400 text-gray-800 px-12 py-6 space-y-8">
-            <div className="w-full">
-              <p className="">
-                Somos un estudio de arquitectura joven que tiene como premisa
-                combinar innovación y excelencia trabajando siempre en equipo.
-              </p>
-            </div>
-            <div className="w-full">
-              <p>
-                Caracterizándonos por la atención personalizada y la supervisión
-                constante en el desarrollo y ejecución de las obras para generar
-                la confianza que nuestros clientes necesitan, aportando el valor
-                agregado que nos diferencia día a día.
-              </p>
-            </div>
-          </div>
-          <div className="w-1/2 h-80 overflow-hidden rounded-lg">
-            <img
-              src="https://res.cloudinary.com/doczyujqf/image/upload/v1704298991/FZ%20OBRAs/jacek-dylag-nhCPOp4A2Xo-unsplash_yahkvb.jpg"
-              alt=""
-              className="w-full object-cover object-center "
-            />
-          </div>
-        </div>
+      <Impulse />
+      <WhyUs />
+      <div className="text-center py-4 max-w-[1223px] mx-auto ">
+        <ContactUs />
       </div>
       <Footer />
     </div>
