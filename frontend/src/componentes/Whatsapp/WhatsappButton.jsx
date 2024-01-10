ded6d9d893920000e916cc564a4dd3ad718bb617
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-01-10 10:58:59 -0300

    num

--- frontend/src/componentes/Whatsapp/WhatsappButton.jsx
+++ frontend/src/componentes/Whatsapp/WhatsappButton.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsAppButton = () => {
   return (
-    <a href="https://wa.me/+529984112504" target="_blank">
+    <a href="https://wa.me/" target="_blank">
       <button
         className="fixed bottom-6 left-6 bg-green-500 hover:bg-green-600 rounded-full h-32 w-32 flex justify-center items-center text-white z-10"
         style={{ height: "58px", width: "58px" }}
